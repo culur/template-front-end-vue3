48d21d9a8d8b709d140f54ac770b49916dfa59c8
Karion <karionnuel@gmail.com>
2022-02-13 23:23:34 +0700

    fix(header): change play btn color

--- src/core/components/layouts/home/HomePlayBtn.vue
+++ src/core/components/layouts/home/HomePlayBtn.vue
@@ -3,7 +3,7 @@ const { t } = useI18n()
 </script>
 
 <template>
-  <StylizedBtn size="lg" color="green" class="justify-center min-w-32">
+  <StylizedBtn size="lg" color="yellow" class="justify-center min-w-32">
     {{ t('button.play') }}
   </StylizedBtn>
 </template>
