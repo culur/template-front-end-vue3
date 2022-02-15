78cc1923583c31d312d8f9cc0ba5cd6fb550de91
thaotram <sawa.kisei@gmail.com>
2022-02-15 21:59:47 +0700

    feat(layout): update header

--- src/core/components/layouts/default/DefaultHeader.vue
+++ src/core/components/layouts/default/DefaultHeader.vue
@@ -1,5 +1,18 @@
+<script setup lang="ts">
+const { t } = useI18n()
+</script>
+
 <template>
   <header>
-    This is default header
+    <Card body-class="flex justify-between p-3">
+      <div class="flex items-center">
+        <div>
+          <span class="font-display mr-1">{{ t('common.balance') }}:</span>
+          <span>0 {{ t('nav.links.lion') }}</span>
+        </div>
+        <img class="h-4 ml-1 w-4" src="/img/token-logo.png">
+      </div>
+      <StylizedBtn>{{ t('button.connect-wallet') }}</StylizedBtn>
+    </Card>
   </header>
 </template>
