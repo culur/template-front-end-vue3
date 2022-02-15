5a226001ec8fc7fdaa3f1ccc19858f55a4921289
thaotram <sawa.kisei@gmail.com>
2022-02-15 21:50:11 +0700

    feat(layout): update index

--- src/play/pages/play/index.vue
+++ src/play/pages/play/index.vue
@@ -1,5 +1,19 @@
+<script setup lang="ts">
+const { t } = useI18n()
+</script>
+
 <template>
-  <section>
-    This is playpage
-  </section>
+  <Card
+    class="h-full"
+    body-class="h-full py-10px px-3"
+    inner-class="h-full"
+    inner-shadow
+  >
+    <div class="flex text-heading items-center">
+      <icon-foundation:paw class="text-3xl" />
+      <div class="font-display ml-2 text-2xl">
+        {{ t('play.my-animals') }}
+      </div>
+    </div>
+  </Card>
 </template>
