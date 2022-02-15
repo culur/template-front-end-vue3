841d0455b9b1cf05bc6c8112edbfd2e7f0720e26
Karion <karionnuel@gmail.com>
2022-02-15 12:42:26 +0700

    feat(config): update community cfg & docs cfg

--- src/core/constants/config.ts
+++ src/core/constants/config.ts
@@ -6,8 +6,14 @@ export const PROJECT_COMMUNITIES = {
   twitter: 'https://twitter.com/animalracebsc',
   medium: 'https://medium.com/@animalracesp',
   telegram: 'https://t.me/animalrace',
-  discord: '#',
+  discord: 'https://discord.gg/4GrnfPGt',
   facebook: 'https://www.facebook.com/Animal-Race-101088619146106',
   youtube: 'https://www.youtube.com/channel/UC4r0dVPD74wVCo9MF1nWMuA',
   instagram: 'https://www.instagram.com/animalracebsc',
+}
+
+export const PROJECT_DOCUMENT = {
+  whitepaper: 'https://www.gitbook.com/',
+  faq: '',
+  tnc: '',
 }
