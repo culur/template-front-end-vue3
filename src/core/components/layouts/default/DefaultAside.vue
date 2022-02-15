6c3e4a02b95534dc8296e7a6eb94cefe53645cd4
thaotram <sawa.kisei@gmail.com>
2022-02-15 21:59:44 +0700

    feat(layout): update aside

--- src/core/components/layouts/default/DefaultAside.vue
+++ src/core/components/layouts/default/DefaultAside.vue
@@ -1,5 +1,56 @@
+<script setup lang="ts">
+const { t } = useI18n()
+</script>
+
 <template>
   <aside>
-    This is aside
+    <Card
+      class="h-full min-w-240px"
+      body-class="h-full overflow-hidden"
+      inner-class="h-full"
+      inner-shadow
+    >
+      <img
+        src="/img/big-logo.png"
+        class="rounded-b-xl object-contain bg-[#282000] h-116px w-full p-2 block"
+      >
+      <div>
+        <DefaultAsideItem selected :text="t('play.my-animals')">
+          <template #icon>
+            <icon-foundation:paw />
+          </template>
+        </DefaultAsideItem>
+        <DefaultAsideItem :text="t('play.box-shop')">
+          <template #icon>
+            <icon-ant-design:gift-filled />
+          </template>
+        </DefaultAsideItem>
+
+        <DefaultAsideItem
+          disabled
+          :text="t('play.animal-staking')"
+          :sub-text="t('play.coming-soon')"
+        >
+          <template #icon>
+            <icon-ph:vault-bold />
+          </template>
+        </DefaultAsideItem>
+        <DefaultAsideItem :text="t('play.racetrack')">
+          <template #icon>
+            <icon-icons8:finish-flag />
+          </template>
+        </DefaultAsideItem>
+        <DefaultAsideItem :text="t('play.maketplace')">
+          <template #icon>
+            <icon-ic:round-store />
+          </template>
+        </DefaultAsideItem>
+        <DefaultAsideItem :text="t('play.history')">
+          <template #icon>
+            <icon-fluent:history-24-filled />
+          </template>
+        </DefaultAsideItem>
+      </div>
+    </Card>
   </aside>
 </template>
---
+++ src/core/components/layouts/default/DefaultAsideItem.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+const { t } = useI18n()
+const props = defineProps({
+  selected: {
+    type: Boolean,
+    default: false,
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  subText: {
+    type: String,
+    default: '',
+  },
+  text: {
+    type: String,
+    default: '',
+  },
+})
+</script>
+
+<template>
+  <button
+    class="aside-item"
+    :class="{
+      'aside-item--selected': selected,
+      'aside-item--disabled': disabled
+    }"
+    :disabled="disabled"
+  >
+    <slot name="icon" />
+    <div class="flex flex-col items-end">
+      <div class="h-2" />
+      <div class="h-19px leading-18px">
+        {{ text }}
+      </div>
+      <div class="h-2 text-8px aside-item__sub-text">
+        {{ subText }}
+      </div>
+    </div>
+  </button>
+</template>
+
+<style scoped>
+.aside-item {
+  @apply flex h-56px w-full px-6 items-center;
+  @apply text-[#1F1F1F];
+
+  & :deep(svg) {
+    @apply text-xl mr-1;
+  }
+
+  &:hover {
+    @apply bg-[#8e5f0414];
+  }
+
+}
+.aside-item__sub-text {
+  @apply text-[#1F1F1F80];
+}
+
+.aside-item--selected {
+  @apply bg-[#8E5F0414];
+
+  box-shadow: 0px -4px 0px 0px #8e5f044d inset;
+}
+
+.aside-item--disabled {
+  @apply opacity-60;
+}
+</style>
