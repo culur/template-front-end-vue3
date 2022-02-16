9310b3af80130facc779e39336b6b1871015fa1b
thaotram <sawa.kisei@gmail.com>
2022-02-16 17:22:02 +0700

    feat(modal): simple usage of modal

--- src/play/pages/play/index.vue
+++ src/play/pages/play/index.vue
@@ -1,19 +1,31 @@
 <script setup lang="ts">
 const { t } = useI18n()
+
+const isShow = ref(false)
 </script>
 
 <template>
-  <Card
-    class="h-full"
-    body-class="h-full py-10px px-3"
-    inner-class="h-full"
-    inner-shadow
-  >
-    <div class="flex text-heading items-center">
-      <icon-foundation:paw class="text-3xl" />
-      <div class="font-display ml-2 text-2xl">
-        {{ t('play.my-animals') }}
-      </div>
+  <div class="flex text-heading items-center">
+    <icon-foundation:paw class="text-3xl" />
+    <div class="font-display ml-2 text-2xl">
+      {{ t('play.my-animals') }}
     </div>
-  </Card>
+    <StylizedBtn class="ml-2" @click="isShow = true">
+      Open modal
+    </StylizedBtn>
+  </div>
+  <Modal v-model="isShow">
+    <template #header>
+      Modal
+    </template>
+    <template #default>
+      content
+    </template>
+    <template #actions="{ close }">
+      <StylizedBtn color="yellow" @click="close">
+        Cancel
+      </StylizedBtn>
+      <StylizedBtn>Proceed</StylizedBtn>
+    </template>
+  </Modal>
 </template>
